1b75a1af40045bc36baadf4155cf2d7ac71895bc
Matheus <math3226@gmail.com>
2023-12-05 12:52:54 -0300

    Switch contidional

--- conditionals.go
+++ conditionals.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-// if and switch accept an initialization statement
 func main() {
-	if test := false; test {
+	if test := true; test {
 		fmt.Println("Hello If")
+	}
+
+	switch hour := time.Now().Hour(); {
+	case hour < 12:
+		fmt.Println("Good morning!")
+	case hour < 17:
+		fmt.Println("Good afternoon!")
+	default:
+		fmt.Println("Good evening!")
+	}
+
+	switch time.Now().Day() {
+	case 5:
+		fmt.Println("Clean your house.")
+		fallthrough
+	case 10:
+		fmt.Println("Buy some wine.")
+		fallthrough
+	case 15:
+		fmt.Println("Visit a doctor.")
+		fallthrough
+	case 25:
+		fmt.Println("Buy some food.")
+		fallthrough
+	case 31:
+		fmt.Println("Party tonight.")
+	default:
+		fmt.Println("No information available for that day.")
 	}
 }
